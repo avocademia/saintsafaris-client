75847007436285d37d300c49e0635bb3f4c898a3
avocademia <jothamoj@gmail.com>
2025-01-30 12:58:53 +0300

    user dashboard update and minor tour page updates

--- src/pages/User DashBoard/UserDashboard.jsx
+++ src/pages/User DashBoard/UserDashboard.jsx
@@ -1,33 +1,33 @@
-import style from "./UserDashboard.module.css"
+
 import { userData, clearUserData } from "../../Helpers"
 import { Link, Navigate } from "react-router-dom"
 import { useState } from "react"
 import logout from "../../hooks/logout"
+import dp from '../../../public/user.png'
+import styles from './UserDashboard.module.css'
+import BlueHeader from '../../components/Blue Header/BlueHeader'
 
 const UserDashboard = () => {
-  const { firstName } = userData() || {}
-  const [redirect, setRedirect] = useState(false)
+  const user = userData() || {}
 
   const handleLogout = () => {
     logout()
     clearUserData()
-    setRedirect(true)
   }
 
-  return (
-    <section className={style.page}>
-      {redirect && <Navigate to="/" replace />}
-      {!firstName ? (
-        <button className={style.logOutBtn}>
-          <Link to="/userauth">Sign In/Up</Link>
-        </button>
-      ) : (
-        <button className={style.logOutBtn} onClick={handleLogout}>
+  return user.firstName? (
+    <section className={styles.page}>
+      <BlueHeader/>
+      <main className={styles.details}>
+        <img src={dp}/>
+        <h1>{user.firstName} {user.surname}</h1>
+        <p>@{user.username}</p>
+        <button className={styles.actionBtn} onClick={handleLogout}>
           Log Out
         </button>
-      )}
+      </main>
     </section>
-  )
+  ) : <Navigate to='/userauth'/>
 }
 
 export default UserDashboard;
